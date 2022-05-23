9da5591fadedf7d936415efb36ac8300b0dfc308
startdusk <522383345@qq.com>
2022-05-23 19:03:09 +0800

    feat✨:  module attributes

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -1,4 +1,14 @@
 defmodule Servy.Handler do
+  @moduledoc """
+  Handles HTTP requests.
+  """
+
+  # 定义常量
+  @pages_path Path.expand("../../pages", __DIR__)
+
+  @doc """
+  Transforms the request into a response.
+  """
   def handle(request) do
     # 函数式编程 管道
     request
@@ -10,6 +20,9 @@ defmodule Servy.Handler do
     |> format_response
   end
 
+  @doc """
+  Logs 404 requests
+  """
   def track(%{status: 404, path: path} = conv) do
     IO.puts("Warning: #{path} is on the loose!")
     conv
@@ -61,7 +74,7 @@ defmodule Servy.Handler do
   end
 
   def route(%{method: "GET", path: "/about"} = conv) do
-    Path.expand("../../pages", __DIR__)
+    @pages_path
     |> Path.join("about.html")
     |> File.read()
     |> handle_file(conv)
