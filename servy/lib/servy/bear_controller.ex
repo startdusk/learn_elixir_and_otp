fe92c15c6960c57aba7f32487ab6927f0b1d80e1
startdusk <522383345@qq.com>
2022-06-05 16:01:11 +0800

    feat✨:  comprehensions

--- servy/lib/servy/bear_controller.ex
+++ servy/lib/servy/bear_controller.ex
@@ -2,12 +2,10 @@ defmodule Servy.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
 
-  defp bear_item(bear) do
-    "<li>#{bear.name} - #{bear.type}</li>"
-  end
+  @templates_path Path.expand("../../templates", __DIR__)
 
   def index(conv) do
-    items =
+    bears =
       Wildthings.list_bears()
       # |> Enum.filter(fn b -> Bear.is_grizzly(b) end)
       # 等价于下面, 提升函数为匿名函数
@@ -18,19 +16,14 @@ defmodule Servy.BearController do
       # 等价于下面, 提升函数为匿名函数
       # |> Enum.sort(&Bear.order_asc_by_name(&1, &2))
       |> Enum.sort(&Bear.order_asc_by_name/2)
-      |> Enum.map(&bear_item/1)
-      |> Enum.join()
 
-    %{
-      conv
-      | status: 200,
-        resp_body: "<ul>#{items}</ul>"
-    }
+    render(conv, "index.eex", bears: bears)
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
-    %{conv | status: 200, resp_body: "<h1>Bear #{bear.id}: #{bear.name}</h1>"}
+
+    render(conv, "show.eex", bear: bear)
   end
 
   def create(conv, params) do
@@ -39,5 +32,15 @@ defmodule Servy.BearController do
       | status: 201,
         resp_body: "Created a #{params["type"]} bear named #{params["name"]}"
     }
+  end
+
+  # \\ [] 等价于bingdings为空，则为空列表(默认参数)
+  defp render(conv, template, bindings \\ []) do
+    content =
+      @templates_path
+      |> Path.join(template)
+      |> EEx.eval_file(bindings)
+
+    %{conv | status: 200, resp_body: content}
   end
 end
